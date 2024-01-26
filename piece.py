b05c1d969a611c1c688e885b5eb1e0936def87d4
v-gope <valentin-27@outlook.com>
2024-01-26 14:47:37 +0100

    classe piece

--- piece.py
+++ piece.py
@@ -1,17 +1,48 @@
 import numpy as np
 
 class piece:
-    """Classe représentant une piece."""
+    '''Classe représentant une piece.'''
     
     def _init_(self, mask):
-        """
+        '''
         Constructeur de la classe piece.
             - mask : mask de la piece provenant du fichier .txt
-        """
-        self.mask= mask
+        '''
+        self.liste_masks = self.initialiser_liste_masks(mask)
     
-    def initialiser_mask(self):
-        """
-        Initialise le mask de la piece.
-        """
-        self.mask = np.zeros((5,5))
+    def initialiser_liste_masks(self,mask):
+        '''
+        Initialise la liste des masks de la piece. 
+        Chaque element correspond a un mask de la piece dans une orientation differente.
+            - liste de tuple (mask_piece, mask_piece_adj, mask_piece_diag)
+            
+            Exemple d'element de la liste pour une piece de taille 3x3 en forme de L :
+                - le masque de la piece :
+                      1 1 0
+                      0 1 0
+                      0 1 0
+                - le masque des cases adjacentes a la piece :
+                    0 1 1 0 0
+                    1 0 0 1 0
+                    0 1 0 1 0
+                    0 1 0 1 0
+                    0 0 1 0 0
+                - le masque des cases diagonales a la piece :
+                    1 0 0 1 0
+                    0 0 0 0 0
+                    1 0 0 0 0
+                    0 0 0 0 0
+                    0 1 0 1 0  
+        '''
+        self.masks = []
+        
+        return self.masks
+
+
+def charger_pieces() -> list[piece] :
+    '''
+    Charge les pieces du jeu Blockus a partir de fichiers
+    '''
+    pieces = []
+    
+    return pieces
