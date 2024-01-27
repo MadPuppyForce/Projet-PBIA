0a8b70a620b806ae73788aebd237a6f1e9ab4044
v-gope <valentin-27@outlook.com>
2024-01-27 14:43:24 +0100

    classe joueur

--- joueur.py
+++ joueur.py
@@ -1,62 +1,25 @@
-class Player:
-    """Classe représentant un joueur."""
+import numpy as np
+from game import Blockus_state
 
-    def __init__(self, name, color, pieces, player_mask):
-        """Constructeur de la classe Joueur."""
-        """ name : nom du joueur"""
-        """ color : couleur du joueur"""
-        """ score : score du joueur"""
-        """ pieces : liste des pieces du joueur, chaque possède sa liste de mask"""
-        """ mask_joueur : mask indiquant les cases occupées par les pieces du joueur et les cases induites qui ne peuvent pas être jouées"""
-        self.name = name
-        self.color = color
-        self.score = 0
-        self.pieces = pieces
-        self.player_mask = player_mask
+class Player():
+    '''
+    Classe représentant un joueur.
+    '''
+    def __init__(self):
+        pass
+    
+    def play(self, state: Blockus_state) -> int:
+        raise NotImplementedError("Please Implement this method")
 
-    def __str__(self):
-        """Méthode spéciale pour afficher un joueur."""
-        return self.name + " (" + self.color + ")"
 
-    def __eq__(self, autre):
-        """Méthode spéciale pour comparer deux joueurs."""
-        return self.name == autre.name and self.color == autre.color
-
-    def get_score(self):
-        """Retourne le score du joueur."""
-        return self.score
+class Player_Random(Player):
+    '''
+    Classe représentant un joueur aléatoire.
+    '''
+    def __init__(self):
+        pass
     
-    def get_pieces(self):
-        """Retourne les pieces du joueur."""
-        return self.pieces
-    
-    def get_mask(self):
-        """Retourne le mask du joueur."""
-        return self.player_mask
-    
-    def add_points(self, points):
-        """Ajoute des points au score du joueur."""
-        self.score += points
-    
-    def remove_points(self, points):
-        """Retire des points au score du joueur."""
-        self.score -= points
-        
-    def extract():
-        raise NotImplementedError()
-        
-        
-    def update_mask(self, coord, mask_piece):
-        """Met à jour le mask du joueur."""
-        """coord : coordonnées de la position du coin supéreiur gauche de la piece jouée"""
-        """mask_piece : mask de la piece jouée"""
-        
-        x = coord[0]
-        y = coord[1]
-        
-        self.player_mask[x][y] = 1
-        
-        for i in range(0, len(self.player_mask)):
-            for j in range(0, len(self.player_mask)):
-                if mask_piece[i][j] == 1:
-                    self.player_mask[x+i][y+j] = 1
+    def play(self, state: Blockus_state) -> int:
+        next_states = state.get_next_states()
+        ind_coup = np.random.randint(len(next_states))
+        return ind_coup
