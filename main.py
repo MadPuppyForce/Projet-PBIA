e41ceeef86014263eb3d1f7ba7d5bcd24d2384e8
MadPuppyForce <marie.griffon27@gmail.com>
2024-02-17 17:59:29 +0100

    euh...je ne suis pas sûre, j'ai "commencé" le main

---
+++ main.py
@@ -0,0 +1,51 @@
+from blockus import blockus_state, piece, blockus, joueur
+import numpy as np
+import os
+
+'''
+Constantes
+'''
+WIDTH = 20  # largeur du plateau de jeu
+HEIGHT = 20 # hauteur du plateau de jeu
+
+
+'''
+Initialisation du blockus
+'''
+
+# Création des pièces
+pieces = []
+
+for file in os.listdir("pieces"):
+    if file.endswith(".txt"):
+        pieces.append(piece.charger_pieces(file))
+
+
+# Création des joueurs
+
+#nb_players = 2   #nombre de joueurs (de 2 à 4)
+player1 = joueur.player_random()
+player2 = joueur.player_random()
+
+players = [player1, player2]
+
+
+# Création du plateau de jeu
+
+blockus = blockus.Blockus(WIDTH,HEIGHT,players,pieces)
+
+
+'''
+Tour de jeu
+'''
+
+while not blockus.current_state.is_terminal():
+    # Affichage de l'état du jeu
+    print(blockus.current_state)
+    
+    blockus.play_turn()
+    
+
+# Calcul du score
+score = blockus.current_state.score()
+print("Score final : ", score)
